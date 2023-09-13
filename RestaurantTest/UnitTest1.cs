e60881121298908526abfccf7970eb0340161f80
Emeka Ewelike <emekaewelike@gmail.com>
2023-09-13 12:50:07 +0100

    second commit

---
+++ RestaurantTest/UnitTest1.cs
@@ -0,0 +1,55 @@
+using AiicoRestaurantApi;
+using AiicoRestaurantApi.Controllers;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework.Internal;
+
+namespace RestaurantTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            var restuarants =  new List<Restaurant>()
+            {
+                new Restaurant
+                {
+                    Id = 1,
+                    Name = "Restaurant A",
+                    Address = "123 Main Street",
+                    Longitude = 10,
+                    Latitude = 20,
+                    distance = 5,
+                    City = "new york"
+                },
+                new Restaurant
+                {
+                    Id = 2,
+                    Name = "Restaurant B",
+                    Address = "456 Elm Street",
+                    Longitude = 15,
+                    Latitude = 25,
+                    distance = 8,
+                    City = "Lagos"
+                },
+                new Restaurant
+                {
+                    Id = 3,
+                    Name = "Restaurant C",
+                    Address = "789 Oak Street",
+                    Longitude = 20,
+                    Latitude = 30,
+                    distance = 12,
+                    City = "Oyo"
+                }
+            };
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+        }
+    }
+}
